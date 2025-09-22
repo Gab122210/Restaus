3d838907806b56b7963d65454b762191baa5c8fd
Gabriel George <princegeorge720@gmail.com>
2025-09-22 18:29:38 +0100

    Initial commit for Restaus project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <div className="bg-blue-300 my-[5rem] w-[10%] h-[100px] text-white flex items-center justify-center animate-bounce"> 2</div>
+    </div>
+  );
+};
+
+export default App;
