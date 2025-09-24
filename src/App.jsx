3c76f9cad92feb6da29ed26cb59197766f881e51
Gabriel George <princegeorge720@gmail.com>
2025-09-24 17:20:47 +0100

    Initial commit with current components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Home from "./HomePage/Home";
 const App = () => {
   return (
     <div>
+      <Home />
       <div className="bg-blue-300 my-[5rem] w-[10%] h-[100px] text-white flex items-center justify-center animate-bounce"> 2</div>
+
     </div>
   );
 };
---
+++ src/HomePage/Home.jsx
@@ -0,0 +1,28 @@
+
+const Home = () => {
+  return (
+    <>
+      <div className="w-full h-[86px] border-b-[1px] border-opacity-60 border-gray-700 flex items-center justify-around ">
+        <ul className="flex gap-10 text-gray-700 font-semibold">
+          <li><a href="http://">HOME</a></li>
+          <li><a href="http://">ABOUT</a></li>
+          <li><a href="http://">OUR MENU</a></li>
+          <li><a href="http://">CONTACT</a></li>
+          <li><a href="http://">SHOP</a></li>
+        </ul> 
+
+        <p className="font-bold text-[20px]">P</p>
+        
+        <ul className="flex gap-10 text-gray-700 font-semibold">
+          <li><a href="http://">HOME</a></li>
+          <li><a href="http://">ABOUT</a></li>
+          <li><a href="http://">OUR MENU</a></li>
+          <li><a href="http://">CONTACT</a></li>
+          <li><a href="http://">SHOP</a></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Home
