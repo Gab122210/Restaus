21f30efe528d1113d26af88682327aa364898b75
Gabriel George <princegeorge720@gmail.com>
2025-09-30 15:35:11 +0100

    initial commit

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,28 +1,106 @@
+// importation for navigation bar
+/*----------------End-------------------*/
+import { FaAngleDown } from "react-icons/fa6";
+import "./Desktopcss.css";
 
 const Home = () => {
   return (
     <>
-      <div className="w-full h-[86px] border-b-[1px] border-opacity-60 border-gray-700 flex items-center justify-around ">
-        <ul className="flex gap-10 text-gray-700 font-semibold">
-          <li><a href="http://">HOME</a></li>
-          <li><a href="http://">ABOUT</a></li>
-          <li><a href="http://">OUR MENU</a></li>
-          <li><a href="http://">CONTACT</a></li>
-          <li><a href="http://">SHOP</a></li>
-        </ul> 
+      <header className="w-full h-[100px] bg-blue-700 selection-none border-b-gray-300 border-opacity-20 ">
+        <div className="hidden md:hidden lg:flex justify-around items-center w-full h-full">
+          <ul className="flex gap-10 text-white font-Alata font-semibold">
+            <li>
+              <a
+                href="#"
+                className="nav-link"
+              >
+                <div className="relative group inline-block">
+                  <div>
+                    <p className="flex items-center gap-2">
+                       HOME
+                   <FaAngleDown className="text-sm" />
+                    </p>
+                  </div>
+                  <div className="absolute top-full left-0 w-[80px] h-6 z-10"></div>
+                    <div className="dropdown-menu-do ">
+                    <ul className="px-4 py-2 text-white cursor-pointer text-[11px] tracking-wide">
+                      <li>
+                        Reservation
+                      </li>
 
-        <p className="font-bold text-[20px]">P</p>
-        
-        <ul className="flex gap-10 text-gray-700 font-semibold">
-          <li><a href="http://">HOME</a></li>
-          <li><a href="http://">ABOUT</a></li>
-          <li><a href="http://">OUR MENU</a></li>
-          <li><a href="http://">CONTACT</a></li>
-          <li><a href="http://">SHOP</a></li>
-        </ul>
-      </div>
+                    </ul>
+
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link flex">
+                ABOUT
+
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                OUR MENU
+              </a>
+            </li>
+           
+            <li>
+              <a href="#" className="nav-link">
+                <div className="relative group inline-block">
+                  <div>
+                  <p className="flex items-center gap-2">
+                    CONTACT
+                    <FaAngleDown className="text-sm" />
+                  </p>
+                  </div>
+                  <div className="absolute top-full left-0 w-[80px] h-6 z-10"></div>
+                  <div className="dropdown-menu">
+                    <ul className="px-4 py-2 text-white cursor-pointer text-[11px] tracking-wide">
+                      <li>
+                        Reservation
+                      </li>
+
+                    </ul>
+
+                  </div>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="nav-link">
+                SHOP
+              </a>
+            </li>
+          </ul>
+
+          <a href="">
+          <h1 className="font-jost text-white font-lighter text-[67px]">P</h1>
+          </a>
+
+          <ul className="flex gap-10 text-gray-700 font-normal">
+            <li>
+              <a href="#">HOME</a>
+            </li>
+            <li>
+              <a href="#">ABOUT</a>
+            </li>
+            <li>
+              <a href="#">OUR MENU</a>
+            </li>
+            <li>
+              <a href="#">CONTACT</a>
+            </li>
+            <li>
+              <a href="#">SHOP</a>
+            </li>
+          </ul>
+        </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,39 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+       fontFamily: {
+        jost: ['Jost', 'sans-serif'],
+        anton: ['Anton', 'sans-serif'],
+        oswald: ['Oswald', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        alata: ['Alata', 'sans-serif'],
+        cormorant: ['Cormorant Garamond', 'serif'],
+        lora: ['Lora', 'serif'],
+        pacifico: ['Pacifico', 'cursive'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        merriweather: ['Merriweather', 'serif'],
+      },
+
+        textShadow: {
+        glow: "0 0 5px #fff, 0 0 10px #fff",
+      },
+
+          /*----custom-ping---*/
+          animation: {
+        "custom-ping": "custom-ping 3s cubic-bezier(0, 0,0.2, 1) infinite",
+      },
+          
+            keyframes: {
+        "custom-ping": {
+          "75%, 100%": {
+            transform: "scale(2)",
+            opacity: "0",
+          },
+        },
+      },
+        
+    },
   },
   plugins: [],
 }
